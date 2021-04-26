2aafecf752803a3fcb7d2e12fea21d5bdfdfdec9
Janik Schoenberg <janik@schoenberg.name>
2021-04-27 01:55:22 +0200

    Fixed ingame Sprite Change Bug

--- Assets/01 Running Game/Scripts/CharacterSelect.cs
+++ Assets/01 Running Game/Scripts/CharacterSelect.cs
@@ -1,22 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Scripts;
 using UnityEngine;
 
 public class CharacterSelect : MonoBehaviour
 {
-    
     [SerializeField] GameObject characterSpriteModel;
     [SerializeField] private Sprite[] characterSprite;
-    
-    private int indexOfSpriteArray=0;
+    [SerializeField] private RunGameController gameControllerInstance;
+
+    private int indexOfSpriteArray = 0;
 
     // Gets the SpriteModel's SpriteRenderer and sets the new Sprite
     public void UpdateSpriteModel(int increment)
     {
-        indexOfSpriteArray += increment;
-        indexOfSpriteArray = indexOfSpriteArray < 0 ? characterSprite.Length-1 : indexOfSpriteArray;
-        indexOfSpriteArray = indexOfSpriteArray >= characterSprite.Length ?  0 : indexOfSpriteArray;
-        characterSpriteModel.GetComponent<SpriteRenderer>().sprite = characterSprite[indexOfSpriteArray];
-        Debug.Log(indexOfSpriteArray);
+        if (!gameControllerInstance.hasGameStarted && !Input.anyKey)
+        {
+            indexOfSpriteArray += increment;
+            indexOfSpriteArray = indexOfSpriteArray < 0 ? characterSprite.Length - 1 : indexOfSpriteArray;
+            indexOfSpriteArray = indexOfSpriteArray >= characterSprite.Length ? 0 : indexOfSpriteArray;
+            characterSpriteModel.GetComponent<SpriteRenderer>().sprite = characterSprite[indexOfSpriteArray];
+        }
     }
 }
